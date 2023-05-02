70906b0d31f8e68d77323a2c9e3b73e2235c8b31
Juanite <juan13_barsa@hotmail.com>
2023-05-02 03:13:31 +0200

    v0.2 - Added some Game management functionalities

--- src/main/java/com/juanite/test/Test.java
+++ src/main/java/com/juanite/test/Test.java
@@ -1,27 +1,9 @@
 package com.juanite.test;
 
-import com.juanite.model.DAO.AdminDAO;
 import com.juanite.model.domain.Admin;
-import com.juanite.model.domain.RepoAdmin;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        //Admin admin = new Admin();
-        Admin admin = new Admin("juanite@boared.com", "Juanite", "Juanite1234");
-        if(admin.save() != null){
-            System.out.println(admin.getName());
-        }else{
-            System.out.println("Error");
-        }
-        /*if(admin.remove() != null){
-            System.out.println(admin.getName());
-        }else{
-            System.out.println("Error");
-        }*/
-        /*if(admin.create("juanite@boared.com", "Juanite", "1234") != null){
-            System.out.println(admin.getName());
-        }else{
-            System.out.println("Error");
-        }*/
+
     }
 }
